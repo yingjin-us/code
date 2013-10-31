a2eed78d6cc0b6ebdd50c970aa216cab811dc82d
yingjin-us <yingjin.us@gmail.com>
2013-10-31 14:47:26 -0400

    BreezeProposalRoundController server side

---
+++ MvcWMS/MvcWMS/Controllers/BreezeProposalRoundController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Breeze.WebApi;
+using Newtonsoft.Json.Linq;
+using MvcWMS.Models;
+using MvcWMS.Models.Contracts;
+
+namespace MvcWMS.Controllers
+{
+    [BreezeController]
+    public class BreezeProposalRoundController : ApiController
+    {
+        IBreezeProposalRoundRepository repository;
+        public BreezeProposalRoundController(IBreezeProposalRoundRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IQueryable<ProposalRound> GetProposalRounds()
+        {
+            return repository.ProposalRounds;
+        }
+
+        // ~/api/BreezeProposalRound/Metadata   
+        [HttpGet]
+        public string Metadata()
+        {
+            return repository.Metadata();
+        }
+
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return repository.SaveChanges(saveBundle);
+        }  
+    }
+}
